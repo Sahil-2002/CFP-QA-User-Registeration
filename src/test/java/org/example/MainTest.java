2571aa5849453869215a9ecb6b4be0b0f4672ba3
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-16 15:35:42 +0530

    testing Various valid email patterns

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -17,22 +17,27 @@ class MainTest {
         String name = "Sahil";
         assertFalse(Main.isvalidname(name));
     }
+
     @Test
-    public void checkemail(){
-        assertTrue(Main.validemail("Sahila.khandekar@gmail.com"));
-        assertTrue(Main.validemail("abc.xyz@bl.co.in"));
-        assertFalse(Main.validemail("sahil@gmail"));
+    public void checkemail() {
+        assertTrue(Main.validemail("abc@yahoo.com"));
+        assertTrue(Main.validemail("abc.100@yahoo.com"));
+        assertTrue(Main.validemail("abc@gmail.com.com"));
+
+        assertFalse(Main.validemail("abc()*@gmail.com"));
 
     }
+
     @Test
-    public void checknumber(){
+    public void checknumber() {
         assertTrue(Main.validnumber("91 9082164605"));
         assertFalse(Main.validnumber("9082164605"));
     }
-@Test
-    public void checkpassword(){
+
+    @Test
+    public void checkpassword() {
         assertTrue(Main.validpassword("Sahil@khandekar17"));
         assertFalse(Main.validpassword("Sahil"));
-}
+    }
 
 }
